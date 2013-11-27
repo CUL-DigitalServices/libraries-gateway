01134075a53a3f084e3e44327f91a2d015df68d0
Tim De Groote <td324@caret.cam.ac.uk>
2013-11-27 14:30:51 +0000

    Add Grunt configuration for the production build, fix project-light stylesheets, move project-light images to '/public/images/interface', only include the css for project-light components we use.

--- GruntFile.js
+++ GruntFile.js
@@ -1,59 +1,184 @@
 module.exports = function (grunt) {
     'use strict';
 
-    var lessFiles = {
-        'public/styles/libraries-gateway.css': ['public/less/libraries-gateway.less']
-    };
+    var lessFiles = [
+        'public/components/project-light/stylesheets/base.css',
+        'public/components/project-light/stylesheets/color-themes.css',
+        'public/components/project-light/stylesheets/carousel.css',
+        'public/components/project-light/stylesheets/teasers.css',
+        'public/less/libraries-gateway.less'
+    ];
 
     grunt.initConfig({
-        watch: {
-            less: {
-                files: ['public/less/**/*.less'],
-                tasks: ['recess:compile']
+        'watch': {
+            'less': {
+                'files': ['public/less/**/*.less'],
+                'tasks': ['less:dev']
             },
-            jshint: {
-                files: [
+            'jshint': {
+                'files': [
                     'public/scripts/**/*.js',
                     'lib/**/*.js'
                 ],
-                tasks: [
+                'tasks': [
                     'jshint:app',
                     'jshint:backend'
                 ]
             }
         },
-        recess: {
-            options: {
-                compile: true
-            },
-            compile: {
-                files: lessFiles
-            },
-            lint: {
-                options: {
-                    compile: false
-                },
-                files: lessFiles
+        'copy': {
+            'build': {
+                'files': [{
+                    'expand': true,
+                    'dest': '<%= outputDir %>',
+                    'src': [
+                        './**',
+                        '!./public/**'
+                    ]
+                }]
             }
         },
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc'
+        'requirejs': {
+            'build': {
+                'options': {
+                    'baseUrl': 'public/scripts',
+                    'out': '<%= outputDir %>/public/scripts/app.min.js',
+                    'name': '../components/requirejs/require',
+                    'mainConfigFile': 'public/scripts/main.js',
+                    'include': [
+                        'main',
+                        'view/page/find-a-library',
+                        'view/page/library-profile',
+                        'view/page/using-our-libraries'
+                    ],
+                    'insertRequire': ['main'],
+                    'wrap': true
+                }
+            }
+        },
+        'imagemin': {
+            'build': {
+                'files': [{
+                    'expand': true,
+                    'cwd': 'public/images',
+                    'src': ['**/*.{jpg,png,gif}'],
+                    'dest': '<%= outputDir %>/public/images'
+                }]
+            }
+        },
+        'less': {
+            'dev': {
+                'files': {
+                    'public/styles/libraries-gateway.css': lessFiles
+                }
             },
-            app: {
-                src: [
+            'build': {
+                'options': {
+                    'compress': true,
+                    'cleancss': true
+                },
+                'files': {
+                    '<%= outputDir %>/public/styles/libraries-gateway.css': lessFiles
+                }
+            }
+        },
+        'rev': {
+            'build': {
+                'files': {
+                    'src': [
+                        '<%= outputDir %>/public/**/*.{js,css,png,jpg}'
+                    ]
+                }
+            }
+        },
+        'clean': {
+            'options': {
+                'force': true
+            },
+            'build': ['<%= outputDir %>']
+        },
+        'jshint': {
+            'options': {
+                'jshintrc': '.jshintrc'
+            },
+            'app': {
+                'src': [
+                    'GruntFile',
                     'public/scripts/**/*.js'
                 ]
             },
-            backend: {
-                src: [
+            'backend': {
+                'src': [
                     'lib/**/*.js'
                 ]
+            }
+        },
+        'usemin': {
+            'stylesheets': {
+                'options': {
+                    'type': 'css',
+                    'dirs': ['<%= outputDir %>/public']
+                },
+                'src': ['<%= outputDir %>/public/styles/**/*.css']
+            },
+            'markup': {
+                'options': {
+                    'type': 'html',
+                    'assetsDirs': ['<%= outputDir %>']
+                },
+                'src': ['<%= outputDir %>/lib/views/**/*.ejs']
+            }
+        },
+        'replace': {
+            'build': {
+                'replacements': [
+                    {
+                        'from': 'components/requirejs/require.js',
+                        'to': 'scripts/app.min.js'
+                    },
+                    {
+                        'from': ' data-main="/public/scripts/main.js"',
+                        'to': ''
+                    }
+                ],
+                'src': ['<%= outputDir %>/lib/views/index.ejs'],
+                'dest': '<%= outputDir %>/lib/views/index.ejs'
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-recess');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-text-replace');
+    grunt.loadNpmTasks('grunt-rev');
+
+    grunt.registerTask('build', function(outputDir) {
+        grunt.config.set('outputDir', outputDir || './dist')
+        // Empty the dist folder
+        grunt.task.run('clean:build');
+        // Copy all the files to this folder except for '/public'
+        grunt.task.run('copy:build');
+        // Compile all less to 'dist/public/styles'
+        grunt.task.run('less:build');
+        // Minify all javascript to 'dist/public/scripts/app.min.js'
+        grunt.task.run('requirejs:build');
+        // Copy and optimize all images to 'dist/public/images'
+        grunt.task.run('imagemin:build');
+        // Generate unique hashes for each file
+        grunt.task.run('rev:build');
+        // Replace the requirejs script tag in 'index.ejs' with a script tag
+        // pointing to 'app.min.js'
+        grunt.task.run('replace:build');
+        // Update the asset paths in the templates to use the correct hashed
+        // filenames
+        grunt.task.run('usemin:markup');
+        // Update the asset paths in the css to use the correct hashed filenames
+        grunt.task.run('usemin:stylesheets');
+    });
 };
