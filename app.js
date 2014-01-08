3c20b60962ffb4f7b1d04072b93f376d3d073dc5
Mathieu Decoene <hello@mathieudecoene.be>
2014-01-08 13:25:23 +0000

    Moved routes to server util

--- app.js
+++ app.js
@@ -5,56 +5,10 @@
  */
 var init = function() {
 
-    // Create a new express server
-    // @see /lib/controllers/server
-    require('./lib/util/server').createServer(__dirname, function(app) {
-
-        ////////////////
-        // API routes //
-        ////////////////
-
-        // This displays an instance of the Swagger UI
-        app.get('/api', function(req, res) {
-            res.sendfile(__dirname + '/public/swagger/index.html');
-        });
-
-        app.get('/api/libraries', require('./lib/controllers/api/libraries').getLibraries);
-        app.get('/api/libraries/:slug', require('./lib/controllers/api/libraries').getLibraryBySlug);
-        app.get('/api/search', require('./lib/controllers/api/search').getCombinedResults);
-        app.get('/api/search/:api', require('./lib/controllers/api/search').getResultsFromIndex);
-
-        ///////////////////
-        // Client routes //
-        ///////////////////
-
-        // Home
-        app.get('/', require('./lib/controllers/client/nodes/home').getContent);
-
-        // Blog
-        app.get('/blogs', require('./lib/controllers/client/nodes/blogs').getContent);
-
-        // Find a library
-        app.get('/find-a-library', require('./lib/controllers/client/nodes/libraries').getContent);
-        app.get('/find-a-library/:id', require('./lib/controllers/client/nodes/libraries').getLibraryDetail);
-
-        // Find a resource
-        app.get('/find-a-resource',  require('./lib/controllers/client/nodes/resources').getContent);
-        app.get('/find-a-resource/:api',  require('./lib/controllers/client/nodes/resources').getResourceDetail);
-        app.get('/find-a-resource/:api/:id',  require('./lib/controllers/client/nodes/resources').getResourceDetail);
-
-        // My account
-        app.get('/my-account', require('./lib/controllers/client/nodes/account').getContent);
-
-        // Using our libraries
-        app.get('/using-our-libraries', require('./lib/controllers/client/nodes/usinglibraries').getContent);
-
-        ////////////
-        // ERRORS //
-        ////////////
-
-        // ErrorHandler
-        app.use(require('./lib/controllers/error/errorHandler').getErrorPage);
-    });
+    /**
+     * Create a new express server
+     */
+    require('./lib/util/server').createServer();
 };
 
 init();
