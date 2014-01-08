5fa62e46b21e6f285381e8d383b05c8ff938b44d
Tim De Groote <td324@caret.cam.ac.uk>
2014-01-08 10:55:51 +0000

    Make facet groups collapsible

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -5,14 +5,15 @@ define([], function() {
             'milesToMetres': 1609.344,
             'streetViewRadius': 50
         },
+        'imagePaths': {
+            'directionsMarker': '/public/images/directions-icon.png'
+        },
         'pages': [
             'find-a-library',
+            'find-a-resource-results',
             'library-profile',
             'resource-detail',
             'using-our-libraries'
-        ],
-        'imagePaths': {
-            'directionsMarker': '/public/images/directions-icon.png'
-        }
+        ]
     };
 });
